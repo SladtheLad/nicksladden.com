8b0778031b4b5bd22bbd006ddfbc23c5eebf9e8c
Nick Sladden <nickoliasxii@gmail.com>
2024-01-31 09:06:07 -0600

    add analytics the ASTRO way??

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable React to support React JSX components.
-	integrations: [react()],
+  // Enable React to support React JSX components.
+  integrations: [react()],
+  output: 'server',
+  adapter: vercel({ webAnalytics: { enabled: true } }),
 });
