f1d3efc8d4381395e51643d246c5b7d253a03906
Nick Sladden <nickoliasxii@gmail.com>
2024-03-12 10:34:26 -0500

    make typewriter effect for just first card and some style edits

--- src/components/rpg-dialog.tsx
+++ src/components/rpg-dialog.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 interface RPGDialogProps extends React.ComponentProps<'dialog'> {
   cards: string[];
@@ -6,13 +6,34 @@ interface RPGDialogProps extends React.ComponentProps<'dialog'> {
 
 const RPGDialog = ({ cards, children, ...props }: RPGDialogProps) => {
   const [currentCard, setCurrentCard] = React.useState<string>(cards[0] || '');
+  const [currentIndex, setCurrentIndex] = React.useState<number>(0);
+
+  const [currentTypeText, setCurrentTypeText] = React.useState<string>('');
+  const [currentTypeTextIndex, setCurrentTypeTextIndex] =
+    React.useState<number>(0);
+  let delay = 25;
 
   const handleNext = () => {
-    const currentIndex = cards.indexOf(currentCard);
     if (currentIndex < cards.length - 1) {
       setCurrentCard(cards[currentIndex + 1]);
+      setCurrentIndex(currentIndex + 1);
     }
   };
+
+  useEffect(() => {
+    if (currentIndex === 0) {
+      if (currentTypeTextIndex < currentCard.length) {
+        const timeout = setTimeout(() => {
+          setCurrentTypeText(
+            (prevText) => prevText + currentCard[currentTypeTextIndex],
+          );
+          setCurrentTypeTextIndex((prevIndex) => prevIndex + 1);
+        }, delay);
+
+        return () => clearTimeout(timeout);
+      }
+    }
+  }, [currentTypeTextIndex, delay, currentTypeText, currentIndex]);
 
   return (
     <dialog
@@ -22,7 +43,9 @@ const RPGDialog = ({ cards, children, ...props }: RPGDialogProps) => {
       {...props}
       onClick={handleNext}
     >
-      {currentCard}
+      <div id='typed-text'>
+        {currentIndex === 0 ? currentTypeText : currentCard}
+      </div>
       {children}
     </dialog>
   );
