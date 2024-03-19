c6489e81c2a03548c65ffdd7d9aeaefe9f685407
Nick Sladden <nickoliasxii@gmail.com>
2024-03-19 11:53:07 -0500

    add back static gen

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
   integrations: [react()],
-  output: 'server',
+  output: 'static',
   adapter: vercel({ webAnalytics: { enabled: true } }),
 });
